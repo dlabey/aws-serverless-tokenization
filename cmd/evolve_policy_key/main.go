37907165c3269db022a8ec050eb52f1086480696
Darren Labey <dlabey@fastmail.com>
2018-07-03 23:50:51 -0700

    Good evolve_policy_key test

--- cmd/evolve_policy_key/main.go
+++ cmd/evolve_policy_key/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,23 +12,10 @@ import (
 	"gopkg.in/aws/aws-lambda-go.v1/lambda"
 )
 
-// AWS Clients that can be mocked for testing
-var (
-	KMS      = NewKMS()
-	DynamoDB = NewDynamoDB()
-
-	sess = session.Must(session.NewSession())
-)
-
 // KMSAPI is a subset of kmsiface.KMSAPI
 type KMSAPI interface {
 	CreateKey(input *kms.CreateKeyInput) (*kms.CreateKeyOutput, error)
 	GenerateDataKey(input *kms.GenerateDataKeyInput) (*kms.GenerateDataKeyOutput, error)
-}
-
-// NewKMS is a mockable KMS client
-func NewKMS() KMSAPI {
-	return kms.New(sess)
 }
 
 // DynamoDBAPI is a subset of dynamodbiface.DynamoDBAPI
@@ -35,57 +23,58 @@ type DynamoDBAPI interface {
 	PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
 }
 
-// NewDynamoDB is a mockable DynamoDB client
-func NewDynamoDB() DynamoDBAPI {
-	return dynamodb.New(sess)
-}
-
 // EvolvePolicyHandler is a CloudWatch even handler that envolves the policy key.
-func EvolvePolicyHandler(event events.CloudWatchEvent) (string, error) {
-	var out string
+func EvolvePolicyHandler(kmsSvc KMSAPI, dynamodbSvc DynamoDBAPI) func(event events.CloudWatchEvent) (string, error) {
+	return func(event events.CloudWatchEvent) (string, error) {
+		var out string
 
-	kmsSvc := NewKMS()
-	dynamodbSvc := NewDynamoDB()
+		// create a new policy key
+		policyKeyRes, err := kmsSvc.CreateKey(&kms.CreateKeyInput{})
+		if err != nil {
+			return out, err
+		}
 
-	// create a new policy key
-	policyKeyRes, err := kmsSvc.CreateKey(&kms.CreateKeyInput{})
-	if err != nil {
+		// generate the encrypted data key for the new policy key
+		dataKeyInput := &kms.GenerateDataKeyInput{
+			KeyId:   aws.String(*policyKeyRes.KeyMetadata.KeyId),
+			KeySpec: aws.String("AES_256"),
+		}
+		dataKeyRes, err := kmsSvc.GenerateDataKey(dataKeyInput)
+		if err != nil {
+			return out, err
+		}
+
+		// save policy reference in database
+		policyKeyInput := &dynamodb.PutItemInput{
+			TableName: aws.String("PolicyKeys"),
+			Item: map[string]*dynamodb.AttributeValue{
+				"PolicyKeyID": {
+					S: aws.String(*policyKeyRes.KeyMetadata.KeyId),
+				},
+				"DataKey": {
+					B: dataKeyRes.CiphertextBlob,
+				},
+				"CreatedAt": {
+					S: aws.String(time.Now().UTC().Format(time.RFC3339)),
+				},
+			},
+		}
+		_, err = dynamodbSvc.PutItem(policyKeyInput)
+		if err == nil {
+			out = *policyKeyRes.KeyMetadata.KeyId
+		}
+
 		return out, err
 	}
-
-	// generate the encrypted data key for the new policy key
-	dataKeyInput := &kms.GenerateDataKeyInput{
-		KeyId:   aws.String(*policyKeyRes.KeyMetadata.KeyId),
-		KeySpec: aws.String("AES_256"),
-	}
-	dataKeyRes, err := kmsSvc.GenerateDataKey(dataKeyInput)
-	if err != nil {
-		return out, err
-	}
-
-	// save policy reference in database
-	policyKeyInput := &dynamodb.PutItemInput{
-		TableName: aws.String("PolicyKeys"),
-		Item: map[string]*dynamodb.AttributeValue{
-			"PolicyKeyID": {
-				S: aws.String(*policyKeyRes.KeyMetadata.KeyId),
-			},
-			"DataKey": {
-				B: dataKeyRes.CiphertextBlob,
-			},
-			"CreatedAt": {
-				S: aws.String(time.Now().UTC().Format(time.RFC3339)),
-			},
-		},
-	}
-	_, err = dynamodbSvc.PutItem(policyKeyInput)
-	if err == nil {
-		out = *policyKeyRes.KeyMetadata.KeyId
-	}
-
-	return out, err
 }
 
 func main() {
-	lambda.Start(EvolvePolicyHandler)
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("REGION")),
+	}))
+
+	kmsSvc := kms.New(sess)
+	dynamodbSvc := dynamodb.New(sess)
+
+	lambda.Start(EvolvePolicyHandler(kmsSvc, dynamodbSvc))
 }
--- cmd/evolve_policy_key/main_test.go
+++ cmd/evolve_policy_key/main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/stretchr/testify/mock"
@@ -14,50 +15,50 @@ type MockKMS struct {
 	mock.Mock
 }
 
-var createKeyOutput = &kms.CreateKeyOutput{}
-
 func (mock *MockKMS) CreateKey(input *kms.CreateKeyInput) (*kms.CreateKeyOutput, error) {
-	return createKeyOutput, nil
+	args := mock.Called(input)
+
+	return args.Get(0).(*kms.CreateKeyOutput), args.Error(1)
 }
 
-var generateDataKeyOutput = &kms.GenerateDataKeyOutput{}
-
 func (mock *MockKMS) GenerateDataKey(input *kms.GenerateDataKeyInput) (*kms.GenerateDataKeyOutput, error) {
-	return generateDataKeyOutput, nil
+	args := mock.Called(input)
+
+	return args.Get(0).(*kms.GenerateDataKeyOutput), args.Error(1)
 }
 
 type MockDynamoDb struct {
 	mock.Mock
 }
 
-var putItemOutput = &dynamodb.PutItemOutput{}
-
 func (mock *MockDynamoDb) PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
-	return putItemOutput, nil
+	args := mock.Called(input)
+
+	return args.Get(0).(*dynamodb.PutItemOutput), args.Error(1)
 }
 
 func TestHandler(test *testing.T) {
-	oldKMS := KMS
-	oldDynamoDB := DynamoDB
-
-	defer func() { KMS = oldKMS }()
-	defer func() { DynamoDB = oldDynamoDB }()
-
-	KMS := new(MockKMS)
-	DynamoDB := new(MockDynamoDb)
+	kmsSvcMock := new(MockKMS)
+	dynamodbSvcMock := new(MockDynamoDb)
 
 	evt := events.CloudWatchEvent{}
 
-	KMS.On("CreateKey", mock.Anything).Return(createKeyOutput, nil)
-	KMS.On("GenerateDataKey", mock.Anything).Return(generateDataKeyOutput, nil)
-	DynamoDB.On("PutItem", mock.Anything).Return(putItemOutput, nil)
+	kmsSvcMock.On("CreateKey", mock.Anything).Return(&kms.CreateKeyOutput{
+		KeyMetadata: &kms.KeyMetadata{
+			KeyId: aws.String("test"),
+		},
+	}, nil)
+	kmsSvcMock.On("GenerateDataKey", mock.Anything).Return(&kms.GenerateDataKeyOutput{
+		CiphertextBlob: []byte("test"),
+	}, nil)
+	dynamodbSvcMock.On("PutItem", mock.Anything).Return(&dynamodb.PutItemOutput{}, nil)
 
-	out, err := EvolvePolicyHandler(evt)
+	out, err := EvolvePolicyHandler(kmsSvcMock, dynamodbSvcMock)(evt)
 
 	fmt.Println(out)
 
 	fmt.Println(err)
 
-	KMS.AssertExpectations(test)
-	DynamoDB.AssertExpectations(test)
+	kmsSvcMock.AssertExpectations(test)
+	dynamodbSvcMock.AssertExpectations(test)
 }
